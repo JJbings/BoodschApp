715279e17fa291ca74308e47db8478c41aac7403
Joco Bogdanovic <j.bgdnvc@gmail.com>
2021-09-01 12:06:28 +0200

    First commit

---
+++ src/components/TodoItem.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const TodoItem = () => {
+    return (
+        <div>
+        <div>Number</div>
+           <div>Title</div>
+           <div>Amount</div>
+        </div>
+    )
+}
+
+export default TodoItem
